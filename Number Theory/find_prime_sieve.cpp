dee6ff188af4cb9ceecda85d3c91359cf05d5fb5
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-06-15 06:55:36 +0530

    Create find_prime_sieve.cpp

---
+++ Number Theory/find_prime_sieve.cpp
@@ -0,0 +1,39 @@
+bool isprime(int n)
+{
+    if(n==1)
+        return false;
+    
+    for(int i=2;i*i<=n;i++)
+        if(n%i==0)
+            return false;
+    
+    return true;
+}
+
+vector<int> primeNumbersTillN(int N) 
+{ 
+ 	// Write your code here
+    vector<int>prime(N+1);
+    vector<int>ans;
+    
+    for(int i=1;i<=N;i++)
+        prime[i]=true;
+    
+    prime[1]=false;
+    
+    for(int i=2;i*i<=N;i++)
+    {
+        if(isprime(i))
+        {
+            for(int j=i*i;j<=N;j+=i)
+            prime[j]=false;
+        }
+    }
+    
+    for(int i=0;i<=N;i++)
+    {
+        if(prime[i]==true)
+            ans.push_back(i);
+    }
+    return ans;
+}
