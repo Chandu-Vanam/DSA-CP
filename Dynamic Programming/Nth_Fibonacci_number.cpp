b6d6587aff66d77a2d464de8a36fb4f9da997df3
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-06-30 07:39:10 +0530

    Create Nth_Fibonacci_number.cpp

---
+++ Dynamic Programming/Nth_Fibonacci_number.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int solve(int n,int dp[])
+{
+    if(n<=1)
+        return n;
+    
+    if(dp[n]!=-1)
+        return dp[n];
+    else
+    	return solve(n-1,dp)+solve(n-2,dp);
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int dp[n+1]={-1};
+    for(int i=0;i<=n;i++)
+        dp[i]=-1;
+        // cout<<dp[i];
+    
+    cout<<solve(n,dp);
+    return 0;
+}
