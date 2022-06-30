065215a98733186a0fc4e0b237101ef44a846a01
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-06-30 07:47:39 +0530

    Update Nth_Fibonacci_number.cpp

--- Dynamic Programming/Nth_Fibonacci_number.cpp
+++ Dynamic Programming/Nth_Fibonacci_number.cpp
@@ -8,8 +8,9 @@ int solve(int n,int dp[])
     
     if(dp[n]!=-1)
         return dp[n];
-    else
-    	return solve(n-1,dp)+solve(n-2,dp);
+    
+    dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    return dp[n];
 }
 
 int main()
@@ -17,6 +18,7 @@ int main()
     int n;
     cin>>n;
     int dp[n+1]={-1};
+    
     for(int i=0;i<=n;i++)
         dp[i]=-1;
         // cout<<dp[i];
