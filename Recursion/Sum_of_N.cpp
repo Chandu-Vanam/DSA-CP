c2db2ba9d84466ccd8dd16a3af3d99c55bac8f87
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-07-16 15:27:52 +0530

    Create Sum_of_N.cpp

---
+++ Recursion/Sum_of_N.cpp
@@ -0,0 +1,16 @@
+int sum(int input[], int n) {
+  /* Don't write main().
+     Don't read input, it is passed as function argument.
+     Return output and don't print it.
+     Taking input and printing output is handled automatically.
+  */
+    if(n==0)
+        return input[0];
+    
+    static int ans=0;
+    ans+=input[n-1];
+    sum(input,n-1);
+    
+    return ans;
+
+}
