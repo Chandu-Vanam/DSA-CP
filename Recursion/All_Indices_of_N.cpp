a11a3f904d296d556d14948136f1e98f2212666f
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-07-16 15:32:01 +0530

    Create All_Indices_of_N.cpp

---
+++ Recursion/All_Indices_of_N.cpp
@@ -0,0 +1,16 @@
+
+void findIndexes(int input[],int currIndex, int size, int x, int output[],int &k) {
+    if(currIndex==size)
+        return;
+    if(input[currIndex]==x){
+        output[k]=currIndex;
+        k++;
+    }
+    findIndexes(input,currIndex+1,size,x,output,k);
+}
+
+int allIndexes(int input[], int size, int x, int output[]) {
+   int k=0;
+    findIndexes(input,0,size,x,output,k);
+    return k;
+}
