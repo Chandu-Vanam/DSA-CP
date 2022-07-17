eafd8a18d1a5ccf0ac9fd78faf9515349d50609a
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-07-17 13:31:23 +0530

    Create Maximum_length_pair.cpp

---
+++ C++_STL_2/Maximum_length_pair.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+bool cmp(pair<int,int>&a,pair<int,int>&b)
+{
+    return a.second<b.second;
+}
+
+int maxLengthChain(vector<pair<int, int>> &p, int n) {
+    // Write your code here.
+    	if(n==1)
+            return 1;
+    
+    sort(p.begin(),p.end(),cmp);
+    
+    int i=0,j=1,cnt=1;
+    while(j<n)
+    {
+        if(p[i].second<p[j].first)
+        {
+        	cnt++;
+            i=j;
+            j++;
+        }
+        else
+            j++;
+    }
+    
+    return cnt;
+}
