c6d2b86dabfd101a7907516b0c0a97be80bc6131
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-07-17 13:36:28 +0530

    Create Longest_substring_with_k.cpp

---
+++ Two_ptr/Longest_substring_with_k.cpp
@@ -0,0 +1,38 @@
+int getLengthofLongestSubstring(int k, string s)
+{
+   // Write your code here.
+    int st=0,total=0,maxi=0;
+    int freq[26]={0};
+    
+    for(int e=0;e<s.size();e++)
+    {
+        if(freq[s[e]-'a']==0)
+        {
+            freq[s[e]-'a']++;
+            total++;
+        }
+        else
+        {
+            freq[s[e]-'a']++;
+        }
+        
+        if(total<=k)
+        {
+            maxi=max(maxi,e-st+1);
+        }
+        else
+        {
+            while(st<s.size() && total>k)
+            {
+                freq[s[st]-'a']--;
+                
+                if(freq[s[st]-'a']==0)
+                    total--;
+                
+                st++;
+            }
+        }
+        
+    }
+    return maxi;
+}
