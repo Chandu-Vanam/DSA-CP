904eaa3c9e9463691379798cfde34b81602abd33
Chandu-Vanam <106000035+Chandu-Vanam@users.noreply.github.com>
2022-07-17 13:25:13 +0530

    Create k-Largest.cpp

---
+++ C++_STL_1/k-Largest.cpp
@@ -0,0 +1,20 @@
+#include<queue>
+
+vector<int> kLargest(int arr[], int n, int k) {
+    // Write your code here
+    priority_queue<int>pq;
+    vector<int>ans;
+    
+    for(int i=0;i<n;i++)
+    {
+        pq.push(arr[i]);
+    }
+    
+    while(k--)
+    {
+        ans.push_back(pq.top());
+        pq.pop();
+    }
+    
+    return ans;
+}
